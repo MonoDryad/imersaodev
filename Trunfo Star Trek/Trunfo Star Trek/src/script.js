7cc9abb9af1cf0eec52a0910bf9ed1c9daffa40e
Rai Raul <87578739+MonoDryad@users.noreply.github.com>
2021-09-22 22:06:34 -0300

    Add files via upload

---
+++ Trunfo Star Trek/Trunfo Star Trek/src/script.js
@@ -0,0 +1,574 @@
+// ------------------Cards----------------- \\
+
+var a1 = {
+  nome: "Jean-Luc Picard",
+  imagem:
+    "https://raw.githubusercontent.com/MonoDryad/imersaodev/main/Trunfo%20Star%20Trek/Imagens/picard-fundo.jpg",
+  atributos: {
+    Hostilidade: 213,
+    Influencia: 433,
+    Diplomacia: 294,
+    Intelecto: 321,
+    Destreza: 0
+  }
+};
+
+var a2 = {
+  nome: "William Thomas Riker",
+  imagem:
+    "https://stt.wiki/w/images/thumb/b/b3/Commander_Riker_Head.png/180px-Commander_Riker_Head.png",
+  atributos: {
+    Hostilidade: 213,
+    Influencia: 433,
+    Diplomacia: 294,
+    Intelecto: 321,
+    Destreza: 0
+  }
+};
+
+var a3 = {
+  nome: "Geordi La Forge",
+  imagem:
+    "https://stt.wiki/w/images/thumb/8/80/Chief_Engineer_La_Forge_Head.png/100px-Chief_Engineer_La_Forge_Head.png",
+  atributos: {
+    Hostilidade: 213,
+    Influencia: 433,
+    Diplomacia: 294,
+    Intelecto: 321,
+    Destreza: 0
+  }
+};
+
+var a4 = {
+  nome: "Worf",
+  imagem:
+    "https://stt.wiki/w/images/thumb/c/cf/Security_Chief_Worf_Head.png/180px-Security_Chief_Worf_Head.png",
+  atributos: {
+    Hostilidade: 213,
+    Influencia: 433,
+    Diplomacia: 294,
+    Intelecto: 321,
+    Destreza: 0
+  }
+};
+
+var a5 = {
+  nome: "Dr. Beverly Crusher",
+  imagem:
+    "https://stt.wiki/w/images/thumb/a/ad/CMO_Crusher_Head.png/100px-CMO_Crusher_Head.png",
+  atributos: {
+    Hostilidade: 213,
+    Influencia: 433,
+    Diplomacia: 294,
+    Intelecto: 321,
+    Destreza: 0
+  }
+};
+
+var a6 = {
+  nome: "Deanna Troi",
+  imagem:
+    "https://stt.wiki/w/images/thumb/8/84/Counselor_Troi_Head.png/100px-Counselor_Troi_Head.png",
+  atributos: {
+    Hostilidade: 213,
+    Influencia: 433,
+    Diplomacia: 294,
+    Intelecto: 321,
+    Destreza: 0
+  }
+};
+
+var a7 = {
+  nome: "Data",
+  imagem:
+    "https://stt.wiki/w/images/thumb/0/0b/Lt._Commander_Data_Head.png/100px-Lt._Commander_Data_Head.png",
+  atributos: {
+    Hostilidade: 213,
+    Influencia: 433,
+    Diplomacia: 294,
+    Intelecto: 321,
+    Destreza: 0
+  }
+};
+
+var a8 = {
+  nome: "Wesley Crusher",
+  imagem:
+    "https://stt.wiki/w/images/thumb/d/dc/Wesley_Crusher_Head.png/100px-Wesley_Crusher_Head.png",
+  atributos: {
+    Hostilidade: 213,
+    Influencia: 433,
+    Diplomacia: 294,
+    Intelecto: 321,
+    Destreza: 0
+  }
+};
+
+var b9 = {
+  nome: "Tasha Yar",
+  imagem:
+    "https://stt.wiki/w/images/thumb/8/81/Lt._Tasha_Yar_Head.png/100px-Lt._Tasha_Yar_Head.png",
+  atributos: {
+    Hostilidade: 213,
+    Influencia: 433,
+    Diplomacia: 294,
+    Intelecto: 321,
+    Destreza: 0
+  }
+};
+
+var b10 = {
+  nome: "Guinan",
+  imagem:
+    "https://stt.wiki/w/images/thumb/c/c6/Fierce_Guinan_Head.png/100px-Fierce_Guinan_Head.png",
+  atributos: {
+    Hostilidade: 213,
+    Influencia: 433,
+    Diplomacia: 294,
+    Intelecto: 321,
+    Destreza: 0
+  }
+};
+
+var b11 = {
+  nome: "Q",
+  imagem:
+    "https://stt.wiki/w/images/thumb/e/e0/Captain_Q_Head.png/100px-Captain_Q_Head.png",
+  atributos: {
+    Hostilidade: 213,
+    Influencia: 433,
+    Diplomacia: 294,
+    Intelecto: 321,
+    Destreza: 0
+  }
+};
+
+var b12 = {
+  nome: "Hugh",
+  imagem:
+    "https://stt.wiki/w/images/thumb/7/78/Hugh_Head.png/100px-Hugh_Head.png",
+  atributos: {
+    Hostilidade: 213,
+    Influencia: 433,
+    Diplomacia: 294,
+    Intelecto: 321,
+    Destreza: 0
+  }
+};
+
+var b13 = {
+  nome: "Spock",
+  imagem:
+    "https://stt.wiki/w/images/thumb/8/80/Ambassador_Spock_Head.png/100px-Ambassador_Spock_Head.png",
+  atributos: {
+    Hostilidade: 213,
+    Influencia: 433,
+    Diplomacia: 294,
+    Intelecto: 321,
+    Destreza: 0
+  }
+};
+
+var b14 = {
+  nome: "Kahless",
+  imagem:
+    "https://stt.wiki/w/images/thumb/1/13/Kahless_the_Unforgettable_Head.png/100px-Kahless_the_Unforgettable_Head.png",
+  atributos: {
+    Hostilidade: 213,
+    Influencia: 433,
+    Diplomacia: 294,
+    Intelecto: 321,
+    Destreza: 0
+  }
+};
+
+var b15 = {
+  nome: "Kirk",
+  imagem:
+    "https://stt.wiki/w/images/thumb/a/ae/Captain_Kirk_Head.png/120px-Captain_Kirk_Head.png",
+  atributos: {
+    Hostilidade: 213,
+    Influencia: 433,
+    Diplomacia: 294,
+    Intelecto: 321,
+    Destreza: 0
+  }
+};
+
+var b16 = {
+  nome: "Kira",
+  imagem:
+    "https://stt.wiki/w/images/thumb/9/90/Commander_Kira_Nerys_Head.png/200px-Commander_Kira_Nerys_Head.png",
+  atributos: {
+    Hostilidade: 213,
+    Influencia: 433,
+    Diplomacia: 294,
+    Intelecto: 321,
+    Destreza: 0
+  }
+};
+
+var c17 = {
+  nome: "Chekov",
+  imagem:
+    "https://stt.wiki/w/images/thumb/b/b3/Lieutenant_Chekov_Head.png/200px-Lieutenant_Chekov_Head.png",
+  atributos: {
+    Hostilidade: 213,
+    Influencia: 433,
+    Diplomacia: 294,
+    Intelecto: 321,
+    Destreza: 0
+  }
+};
+
+var c18 = {
+  nome: "Khan",
+  imagem:
+    "https://stt.wiki/w/images/thumb/f/fe/Wrathful_Khan_Head.png/200px-Wrathful_Khan_Head.png",
+  atributos: {
+    Hostilidade: 213,
+    Influencia: 433,
+    Diplomacia: 294,
+    Intelecto: 321,
+    Destreza: 0
+  }
+};
+
+var c19 = {
+  nome: "Pike",
+  imagem:
+    "https://stt.wiki/w/images/thumb/b/bb/Captain_Pike_Head.png/200px-Captain_Pike_Head.png",
+  atributos: {
+    Hostilidade: 213,
+    Influencia: 433,
+    Diplomacia: 294,
+    Intelecto: 321,
+    Destreza: 0
+  }
+};
+
+var c20 = {
+  nome: "Kira",
+  imagem:
+    "https://stt.wiki/w/images/thumb/e/eb/Lt._Sulu_Head.png/120px-Lt._Sulu_Head.png",
+  atributos: {
+    Hostilidade: 213,
+    Influencia: 433,
+    Diplomacia: 294,
+    Intelecto: 321,
+    Destreza: 0
+  }
+};
+
+var c21 = {
+  nome: "Kira",
+  imagem:
+    "https://stt.wiki/w/images/thumb/9/90/Commander_Kira_Nerys_Head.png/200px-Commander_Kira_Nerys_Head.png",
+  atributos: {
+    Hostilidade: 213,
+    Influencia: 433,
+    Diplomacia: 294,
+    Intelecto: 321,
+    Destreza: 0
+  }
+};
+
+var c22 = {
+  nome: "Kira",
+  imagem:
+    "https://stt.wiki/w/images/thumb/9/90/Commander_Kira_Nerys_Head.png/200px-Commander_Kira_Nerys_Head.png",
+  atributos: {
+    Hostilidade: 213,
+    Influencia: 433,
+    Diplomacia: 294,
+    Intelecto: 321,
+    Destreza: 0
+  }
+};
+
+var c23 = {
+  nome: "Kira",
+  imagem:
+    "https://stt.wiki/w/images/thumb/9/90/Commander_Kira_Nerys_Head.png/200px-Commander_Kira_Nerys_Head.png",
+  atributos: {
+    Hostilidade: 213,
+    Influencia: 433,
+    Diplomacia: 294,
+    Intelecto: 321,
+    Destreza: 0
+  }
+};
+
+var c24 = {
+  nome: "Kira",
+  imagem:
+    "https://stt.wiki/w/images/thumb/9/90/Commander_Kira_Nerys_Head.png/200px-Commander_Kira_Nerys_Head.png",
+  atributos: {
+    Hostilidade: 213,
+    Influencia: 433,
+    Diplomacia: 294,
+    Intelecto: 321,
+    Destreza: 0
+  }
+};
+
+var d25 = {
+  nome: "Kira",
+  imagem:
+    "https://stt.wiki/w/images/thumb/9/90/Commander_Kira_Nerys_Head.png/200px-Commander_Kira_Nerys_Head.png",
+  atributos: {
+    Hostilidade: 213,
+    Influencia: 433,
+    Diplomacia: 294,
+    Intelecto: 321,
+    Destreza: 0
+  }
+};
+
+var d26 = {
+  nome: "Kira",
+  imagem:
+    "https://stt.wiki/w/images/thumb/9/90/Commander_Kira_Nerys_Head.png/200px-Commander_Kira_Nerys_Head.png",
+  atributos: {
+    Hostilidade: 213,
+    Influencia: 433,
+    Diplomacia: 294,
+    Intelecto: 321,
+    Destreza: 0
+  }
+};
+
+var d27 = {
+  nome: "Kira",
+  imagem:
+    "https://stt.wiki/w/images/thumb/9/90/Commander_Kira_Nerys_Head.png/200px-Commander_Kira_Nerys_Head.png",
+  atributos: {
+    Hostilidade: 213,
+    Influencia: 433,
+    Diplomacia: 294,
+    Intelecto: 321,
+    Destreza: 0
+  }
+};
+
+var d28 = {
+  nome: "Kira",
+  imagem:
+    "https://stt.wiki/w/images/thumb/9/90/Commander_Kira_Nerys_Head.png/200px-Commander_Kira_Nerys_Head.png",
+  atributos: {
+    Hostilidade: 213,
+    Influencia: 433,
+    Diplomacia: 294,
+    Intelecto: 321,
+    Destreza: 0
+  }
+};
+
+var d29 = {
+  nome: "Kira",
+  imagem:
+    "https://stt.wiki/w/images/thumb/9/90/Commander_Kira_Nerys_Head.png/200px-Commander_Kira_Nerys_Head.png",
+  atributos: {
+    Hostilidade: 213,
+    Influencia: 433,
+    Diplomacia: 294,
+    Intelecto: 321,
+    Destreza: 0
+  }
+};
+
+var d30 = {
+  nome: "Kira",
+  imagem:
+    "https://stt.wiki/w/images/thumb/9/90/Commander_Kira_Nerys_Head.png/200px-Commander_Kira_Nerys_Head.png",
+  atributos: {
+    Hostilidade: 213,
+    Influencia: 433,
+    Diplomacia: 294,
+    Intelecto: 321,
+    Destreza: 0
+  }
+};
+
+var d31 = {
+  nome: "Kira",
+  imagem:
+    "https://stt.wiki/w/images/thumb/9/90/Commander_Kira_Nerys_Head.png/200px-Commander_Kira_Nerys_Head.png",
+  atributos: {
+    Hostilidade: 213,
+    Influencia: 433,
+    Diplomacia: 294,
+    Intelecto: 321,
+    Destreza: 0
+  }
+};
+
+var d32 = {
+  nome: "Kira",
+  imagem:
+    "https://stt.wiki/w/images/thumb/9/90/Commander_Kira_Nerys_Head.png/200px-Commander_Kira_Nerys_Head.png",
+  atributos: {
+    Hostilidade: 213,
+    Influencia: 433,
+    Diplomacia: 294,
+    Intelecto: 321,
+    Destreza: 0
+  }
+};
+
+// ------------------Var----------------- \\
+
+var cartas = [
+  a1,
+  a2,
+  a3,
+  a4,
+  a5,
+  a6,
+  a7,
+  a8,
+  b9,
+  b10,
+  b11,
+  b12,
+  b13,
+  b14,
+  b15,
+  b16,
+  c17,
+  c18,
+  c19,
+  c20,
+  c21,
+  c22,
+  c23,
+  c24,
+  d25,
+  d26,
+  d27,
+  d28,
+  d29,
+  d30,
+  d31,
+  d32
+];
+
+var cartaMaquina;
+var cartaJogador;
+
+// ------------------Function Maquina----------------- \\
+
+function sortearCarta() {
+  var numeroCartaMaquina = parseInt(Math.random() * 30);
+  cartaMaquina = cartas[numeroCartaMaquina];
+
+  // ------------------Function Jogador----------------- \\
+
+  var numeroCartaJogador = parseInt(Math.random() * 30);
+  while (numeroCartaMaquina == numeroCartaJogador) {
+    var numeroCartaJogador = parseInt(Math.random() * 30);
+  }
+  cartaJogador = cartas[numeroCartaJogador];
+  console.log(cartaJogador);
+  console.log(numeroCartaJogador);
+
+  // ------------------Botões------------------- \\
+  document.getElementById("btnSortear").disabled = true;
+  document.getElementById("btnJogar").disabled = false;
+
+  exibirOpcoes();
+  exibirCartaJogador();
+}
+
+// ------------------Function 3------------ \\
+
+function exibirOpcoes() {
+  var opcoes = document.getElementById("opcoes");
+  var opcoesTexto = "";
+
+  //   for (var atributos in cartaJogador.atributos) {
+  //     opcoesTexto +=
+  //       "<input type='radio' name='atributos' value=" +
+  //       atributos +
+  //       ">" +
+  //       atributos;
+  //   }
+  //   opcoes.innerHTML = opcoesTexto;
+}
+
+// ------------------Function 4------------ \\
+
+function obtemAtributoSelecionado() {
+  var radioAtributos = document.getElementsByName("atributos");
+  for (var i = 0; i < radioAtributos.length; i++) {
+    if (radioAtributos[i].checked == true) {
+      return radioAtributos[i].value;
+    }
+  }
+}
+
+// ------------------Function 5------------ \\
+
+function jogar() {
+  var atributoSelecionado = obtemAtributoSelecionado();
+  var elementoResultado = document.getElementById("resultado");
+  var valorCartaJogador = cartaJogador.atributos[atributoSelecionado];
+  var valorCartaMaquina = cartaMaquina.atributos[atributoSelecionado];
+
+  if (valorCartaJogador > valorCartaMaquina) {
+    elementoResultado.innerHTML = "Vitória!";
+  } else if (valorCartaMaquina > valorCartaJogador) {
+    elementoResultado.innerHTML = "Derrota!";
+  } else {
+    elementoResultado.innerHTML = "Empate!";
+  }
+
+  console.log(cartaMaquina);
+  document.getElementById("btnJogar").disabled = true;
+  document.getElementById("btnSortear").disabled = false;
+  exibirCartaMaquina();
+}
+
+function exibirCartaJogador() {
+  var divCartaJogador = document.getElementById("carta-jogador");
+  divCartaJogador.style.backgroundImage = `url(${cartaJogador.imagem})`;
+  var moldura =
+    '<img src="https://www.alura.com.br/assets/img/imersoes/dev-2021/card-super-trunfo-transparent-ajustado.png" style=" width: inherit; height: inherit; position: absolute;">';
+  var tagHTML = "<div id='opcoes' class='carta-status'>";
+  var opcoesTexto = "";
+
+  for (var atributos in cartaJogador.atributos) {
+    opcoesTexto +=
+      "<input type='radio' name='atributos' value=" +
+      atributos +
+      ">" +
+      atributos +
+      " " +
+      cartaJogador.atributos[atributos] +
+      "<br>";
+  }
+  var nome = `<p class="carta-subtitle">${cartaJogador.nome}</p>`;
+  divCartaJogador.innerHTML = moldura + nome + tagHTML + opcoesTexto + "</div>";
+}
+
+function exibirCartaMaquina() {
+  var divCartaMaquina = document.getElementById("carta-maquina");
+  divCartaMaquina.style.backgroundImage = `url(${cartaMaquina.imagem})`;
+  var moldura =
+    '<img src="https://www.alura.com.br/assets/img/imersoes/dev-2021/card-super-trunfo-transparent-ajustado.png" style=" width: inherit; height: inherit; position: absolute;">';
+  var tagHTML = "<div id='opcoes' class='carta-status1'>";
+  var opcoesTexto = "";
+
+  for (var atributos in cartaMaquina.atributos) {
+    opcoesTexto +=
+      "<p type='text' name='atributos' value=" +
+      atributos +
+      ">" +
+      atributos +
+      " " +
+      cartaMaquina.atributos[atributos] +
+      "</p>";
+  }
+  var nome = `<p class="carta-subtitle">${cartaMaquina.nome}</p>`;
+  divCartaMaquina.innerHTML = moldura + nome + tagHTML + opcoesTexto + "</div>";
+}
